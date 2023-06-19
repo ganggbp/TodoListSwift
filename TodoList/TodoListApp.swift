0f5c56fe781c67719fb18c2941e675f086df35c0
gangg <burit_bk@hotmail.com>
2023-06-19 17:17:28 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Burit Boonkorn on 19/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
